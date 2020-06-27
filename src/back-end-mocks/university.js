7d64b16f1e9a3cdcaf97957d10f1471c959beb47
Giovanni D'amico <giovannidamico@Giovannis-MacBook-Pro.local>
2020-06-27 17:02:09 +0200

    starting on the itemList

--- src/back-end-mocks/university.js
+++ src/back-end-mocks/university.js
@@ -3,18 +3,111 @@ export const items = [
     id: 0,
     name: "Heriot-Watt University",
     imgUrl: "https://hw.ac.uk/img/uk-grid-banner-full.jpg",
+    modules: [
+      {
+        code: "001",
+        title: "Petroleum Engineering",
+      },
+      {
+        code: "CT1",
+        title: "Financial Mathematics",
+      },
+      {
+        code: "CT1",
+        title: "Strategic Management",
+      },
+      {
+        code: "FN1",
+        title: "Foundation of Computer Science I",
+      },
+      {
+        code: "FN2",
+        title: "Foundation of Computer Science II",
+      },
+      {
+        code: "DM2",
+        title: "Discrete Mathematics",
+      },
+      {
+        code: "Blawf1",
+        title: "Accounting",
+      },
+    ],
   },
   {
     id: 1,
     name: "Bocconi",
     imgUrl:
       "https://s3-eu-west-1.amazonaws.com/studeersnel.institution-assets/H8Tl0w3l0kJJxqnZETtY.jpg",
+    modules: [
+      {
+        code: "001",
+        title: "Petroleum Engineering",
+        clicks: 124,
+      },
+      {
+        code: "CT1",
+        title: "Financial Mathematics",
+        clicks: 132,
+      },
+      {
+        code: "CT1",
+        title: "Strategic Management",
+        clicks: 17,
+      },
+      {
+        code: "FN1",
+        title: "Foundation of Computer Science I",
+      },
+      {
+        code: "FN2",
+        title: "Foundation of Computer Science II",
+      },
+      {
+        code: "DM2",
+        title: "Discrete Mathematics",
+      },
+      {
+        code: "Blawf1",
+        title: "Accounting",
+      },
+    ],
   },
   {
     id: 2,
     name: "University of Edinburgh",
     imgUrl:
       "https://scholarship-positions.com/wp-content/uploads/2018/03/University-of-Edinburgh.jpg",
+    modules: [
+      {
+        code: "001",
+        title: "Petroleum Engineering",
+      },
+      {
+        code: "CT1",
+        title: "Financial Mathematics",
+      },
+      {
+        code: "CT1",
+        title: "Strategic Management",
+      },
+      {
+        code: "FN1",
+        title: "Foundation of Computer Science I",
+      },
+      {
+        code: "FN2",
+        title: "Foundation of Computer Science II",
+      },
+      {
+        code: "DM2",
+        title: "Discrete Mathematics",
+      },
+      {
+        code: "Blawf1",
+        title: "Accounting",
+      },
+    ],
   },
   {
     id: 3,
@@ -27,5 +120,52 @@ export const items = [
     name: "King's College",
     imgUrl:
       "https://s3-eu-west-1.amazonaws.com/studeersnel.institution-assets/H8Tl0w3l0kJJxqnZETtY.jpg",
+  },
+];
+
+export const notes = [
+  {
+    title: "Heriot-Watt University - Production Technology I",
+    year: "2018-2019",
+    university: {
+      name: "Heriot-Watt University",
+    },
+    module: {
+      title: "Accounting",
+      code: "Blawf1",
+    },
+  },
+  {
+    title: "3 Loans and Loan Schedules",
+    year: "2018-2019",
+    university: {
+      name: "Heriot-Watt University",
+    },
+    module: {
+      title: "Accounting",
+      code: "Blawf1",
+    },
+  },
+  {
+    title: "Competitive Stance",
+    year: "2018-2019",
+    university: {
+      name: "Heriot-Watt University",
+    },
+    module: {
+      title: "Accounting",
+      code: "Blawf1",
+    },
+  },
+  {
+    title: "Sample/practice exam 6 June, questions and answers",
+    year: "2018-2019",
+    university: {
+      name: "Heriot-Watt University",
+    },
+    module: {
+      title: "Accounting",
+      code: "Blawf1",
+    },
   },
 ];
